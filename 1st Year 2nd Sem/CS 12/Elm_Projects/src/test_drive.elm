1b230a8ec872cc4b262e6b666e9f8a658ec0a998
JacobLaptop <justjacobrosario@gmail.com>
2026-04-13 20:39:30 +0800

    vault backup: 2026-04-13 20:39:30

--- 1st Year 2nd Sem/CS 12/Elm_Projects/src/test_drive.elm
+++ 1st Year 2nd Sem/CS 12/Elm_Projects/src/test_drive.elm
@@ -1,8 +1,8 @@
-module Gridder exposing (..)
+module Test_drive exposing (..)
 
 gridder : Int -> Int -> List (List Int)
 gridder r c =
-	let
-		grid = List.repeat r (List.range 1 c)
-	in
-		grid
+    let
+        grid = List.repeat r (List.range 1 c)
+    in
+        grid
