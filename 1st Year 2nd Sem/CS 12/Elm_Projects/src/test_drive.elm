6b950f235cc44f56368515e642d7ae6c62022dc0
JacobLaptop <justjacobrosario@gmail.com>
2026-04-19 20:15:55 +0800

    vault backup: 2026-04-19 20:15:55

--- 1st Year 2nd Sem/CS 12/Elm_Projects/src/test_drive.elm
+++ 1st Year 2nd Sem/CS 12/Elm_Projects/src/test_drive.elm
@@ -8,26 +8,3 @@ gridder r c =
         grid = List.repeat r (List.range 1 c)
     in
         x
-
-
-evenOnly : List Int -> List Int
-evenOnly list=
-    let
-        new_list = List.filter (\a -> (modBy 2 a) == 0) list
-    in
-        new_list
-
-
-squareEach: List Int -> List Int
-squareEach given_list =
-    let 
-        squared = List.map (\x -> x^2) given_list
-    in
-        squared
-
-filterDivisibleBy: List Int -> Int -> List Int
-filterDivisibleBy given_list n =
-    let
-        div_by_n = List.filter (\x -> (modBy n x) == 0) given_list
-    in
-        div_by_n
