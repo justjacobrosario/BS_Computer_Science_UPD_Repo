1d998a0b48c2ba519eae48faaedc94e35445f011
JacobLaptop <justjacobrosario@gmail.com>
2026-05-04 16:41:18 +0800

    vault backup: 2026-05-04 16:41:18

--- 1st Year 2nd Sem/CS 12/GCI/sample.py
+++ 1st Year 2nd Sem/CS 12/GCI/sample.py
@@ -3,6 +3,9 @@ import numpy.random as random
 import pandas as pd
 from pandas import Series, DataFrame
 
-series = Series([2, 4, 6, 8, 10])
+series = Series(
+	[2, 4, 6, 8, 10],
+	index=[A, B, C, D, E]
+	)
 
 print(series)
