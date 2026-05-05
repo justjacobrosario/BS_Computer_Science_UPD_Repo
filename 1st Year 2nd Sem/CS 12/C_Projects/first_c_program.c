751d6280b4b1bbe5c8a151d328486d551ed48641
JacobLaptop <justjacobrosario@gmail.com>
2026-05-05 10:08:50 +0800

    vault backup: 2026-05-05 10:08:50

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 
-int c[3] = {10, 100, 5};
-int v[3] = {1, 1000, 7};
+
 
 int max_value(int w, int n, const int *c, const int *v) {
-    int res = 0;
+    int res = w;
 
     for (int i = 0; i < n; i++) {
         if (c[i] <= w) {
-            new = w - c[i] + v[i]
+            printf("fahh");
         }
     }
 }
 
-max_value(10, 2, c, v);
+int main(){
+    int c[3] = {10, 100, 5};
+    int v[3] = {1, 1000, 7};
+    max_value(10, 3, c, v);
+}
