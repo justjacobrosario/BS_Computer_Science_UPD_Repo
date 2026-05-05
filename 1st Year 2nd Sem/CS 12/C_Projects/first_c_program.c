19225fa71870a766935db4e1d079570fb453f5ca
JacobLaptop <justjacobrosario@gmail.com>
2026-05-05 10:05:37 +0800

    vault backup: 2026-05-05 10:05:37

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -5,9 +5,13 @@ int c[3] = {10, 100, 5};
 int v[3] = {1, 1000, 7};
 
 int max_value(int w, int n, const int *c, const int *v) {
-    //int c_arr[n] = {c};
-    printf("%d", 1);
-    return 0;
+    int res = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (c[i] <= w) {
+            new = w - c[i] + v[i]
+        }
     }
+}
 
 max_value(10, 2, c, v);
