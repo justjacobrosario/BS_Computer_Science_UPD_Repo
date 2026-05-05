f9008eebc1e6be96af9db6d0982ed21184f4faea
JacobLaptop <justjacobrosario@gmail.com>
2026-05-05 09:27:21 +0800

    vault backup: 2026-05-05 09:27:21

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    int d = (((5 | 1) & 3) ^ 5) << 3;
-    //printf("%d", d);
-    printf("%u", (5<<3));
-}
+
+int c[3] = {10, 100, 5};
+int v[3] = {1, 1000, 7};
+
+int max_value(int w, int n, const int *c, const int *v) {
+    //int c_arr[n] = {c};
+    printf("%d", 1);
+    return 0;
+    }
+
+max_value(10, 2, c, v);
