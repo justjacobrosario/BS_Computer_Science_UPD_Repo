009a281a754f7a2aeb5128414288b522635dac2c
JacobLaptop <justjacobrosario@gmail.com>
2026-05-06 08:14:31 +0800

    vault backup: 2026-05-06 08:14:31

---
+++ 1st Year 2nd Sem/CS 12/GCI/HW2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+# Libraries for retrieving data from the web and handling zip files
+import requests, zipfile
+from io import StringIO
+import io
+
+# Specify the url with data
+url = 'https://github.com/Hernan4444/MyAnimeList-Database/archive/refs/heads/master.zip'
+
+# Acquire data from the url
+r = requests.get(url, stream=True)
+
+# read and extract the zipfile
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+
+# Load and clean the data
+anime_data = pd.read_csv('MyAnimeList-Database-master/data/anime.csv')
+anime_data_extracted = anime_data[anime_data['Score'] != 'Unknown'].copy()
+anime_data_extracted['Score'] = pd.to_numeric(anime_data_extracted['Score'])
+
+
+def homework(anime_data_extracted: pd.DataFrame) -> pd.Series:
+	...
+
+print(anime_data_extracted)
