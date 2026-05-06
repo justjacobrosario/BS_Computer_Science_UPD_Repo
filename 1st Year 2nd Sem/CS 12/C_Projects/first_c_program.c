d0704010ee08f6058b404ea746c5145747cd6a55
JacobLaptop <justjacobrosario@gmail.com>
2026-05-06 17:54:57 +0800

    vault backup: 2026-05-06 17:54:57

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-void hello(){
-    printf("hehe\n");
+char msg[] = {69, 69, 89, 0};
+char yes[] = {'a', 'b', 'c', 'd'};
+
+
+void foo(char *lis, int n){
+    for (int i = 0; i < n; i++){
+        printf("%c", lis[i]);
+    }
 }
 
 int main(){
-    hello();
+    foo(yes, 4);
 }
--- 1st Year 2nd Sem/CS 12/GCI/HW2.py
+++ 1st Year 2nd Sem/CS 12/GCI/HW2.py
@@ -33,68 +33,17 @@ def homework(anime_data_extracted):
 	new = DataFrame(new)
 	return new
 
-print(anime_data_extracted)
+def homework(anime_data_extracted):
+    types = anime_data_extracted["Type"].unique()
+    new = []
+    for type in types:
+        new.append(anime_data_extracted[(anime_data_extracted["Type"] == type)]["Score"].mean())
+    new = pd.Series(sorted(new, reverse=True))
+    return new
+
+
 #print(anime_data_extracted["Type"].unique())
 #print(anime_data_extracted[(anime_data_extracted["Type"] == type)]["Score"].mean())
 
 result = homework(anime_data_extracted)
 print(result)
-
-import ast
-import inspect
-import textwrap
-
-def hw2_public_tests(homework_func):
-    print("=== HW2 Public Tests ===")
-
-    # Get the source code of the homework function
-    source = textwrap.dedent(inspect.getsource(homework_func))
-    tree = ast.parse(source)
-
-    # ---------- Test 1: No import statements ----------
-    print("Test 1: No import statements in function...", end=" ")
-    for node in ast.walk(tree):
-        if isinstance(node, (ast.Import, ast.ImportFrom)):
-            print("NG")
-            if isinstance(node, ast.Import):
-                names = ", ".join(alias.name for alias in node.names)
-            else:
-                names = node.module
-            print(f"  Found import statement: '{names}'")
-            print("  Remove all import statements before submitting.")
-            return
-    print("OK")
-
-    # ---------- Test 2: No data loading code ----------
-    print("Test 2: No data loading code in function...", end=" ")
-    blocked_calls = {"read_csv", "read_excel", "read_json", "read_html",
-                     "get", "post", "ZipFile", "extractall", "urlopen"}
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Call):
-            func_name = ""
-            if isinstance(node.func, ast.Attribute):
-                func_name = node.func.attr
-            elif isinstance(node.func, ast.Name):
-                func_name = node.func.id
-            if func_name in blocked_calls:
-                print("NG")
-                print(f"  Found data loading call: '{func_name}()'")
-                print("  Do not include data downloading/loading code in your function.")
-                return
-    print("OK")
-
-    # ---------- Test 3: No hardcoded file paths or URLs ----------
-    print("Test 3: No hardcoded file paths or URLs...", end=" ")
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Constant) and isinstance(node.value, str):
-            val = node.value
-            if val.startswith("http") or ".csv" in val or ".zip" in val:
-                print("NG")
-                print(f"  Found hardcoded path/URL: '{val[:60]}...'")
-                print("  Do not include file paths or URLs in your function.")
-                return
-    print("OK")
-
-    print("=== All Public Tests Passed ===")
-
-hw2_public_tests(homework)
