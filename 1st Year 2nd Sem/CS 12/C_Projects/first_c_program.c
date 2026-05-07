39f46f635a6957609454fc8656bc2ac4abad4910
JacobLaptop <justjacobrosario@gmail.com>
2026-05-07 15:16:39 +0800

    vault backup: 2026-05-07 15:16:39

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
+
 int main(){
-    char str1[] = "Hello x";
-    char str2[] = {'H', 'e', 'l', 'l', 'o', ' ', 'x', '\0'};
-    char str3[] = {72,101,108,108,111,32,88,0};
+    char x = "k";
+    char *p = x;
 
-    printf("%s\n", str1);
-    printf("%s\n", str2);
-    printf("%s\n", str3);
 
-    int32_t x = 1;
-    int verdict = (x > 0) ? 0 : 1;
-    printf("%d\n", verdict);
+    printf(p);
+
 
 }
