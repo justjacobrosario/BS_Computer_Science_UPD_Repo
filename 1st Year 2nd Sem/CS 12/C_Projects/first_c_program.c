d4cc6a29edacb800cd83d026c688b7674c4861c0
JacobLaptop <justjacobrosario@gmail.com>
2026-05-07 13:08:55 +0800

    vault backup: 2026-05-07 13:08:55

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -2,15 +2,9 @@
 #include <stdint.h>
 
 
-#define N 100
-int16_t x_16_bit = 123456789;
-int32_t y_32_bit = 123456789;
-
+int lis[] = {1, 2, 3};
 
 int main(){
 
-    int32_t widen_x = x_16_bit;
-    int16_t narrowed_y = y_32_bit;
-    printf("x: %d\n", widen_x); 
-    printf("y: %d", narrowed_y); 
+    printf(lis);
 }
