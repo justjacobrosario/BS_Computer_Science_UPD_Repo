cd0dd23ed308ef31a9cc203a7a855e04c53870bc
JacobLaptop <justjacobrosario@gmail.com>
2026-05-07 12:44:49 +0800

    vault backup: 2026-05-07 12:44:49

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 
-char msg[] = {69, 69, 89, 0};
-char yes[] = {'a', 'b', 'c', 'd'};
-
-
-void foo(char *lis, int n){
-    for (int i = 0; i < n; i++){
-        printf("%c", lis[i]);
-    }
-}
+#define N 100
 
 int main(){
-    foo(yes, 4);
+    N--;
+    printf("%d", N);
 }
