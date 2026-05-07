43274023adec028bd3bf8d0b4df2093896f7adfd
JacobLaptop <justjacobrosario@gmail.com>
2026-05-07 13:02:25 +0800

    vault backup: 2026-05-07 13:02:24

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
+
 
 #define N 100
-int eyy = 10;
+int32_t x = 10;
+int64_t y = 50;
+
 
 int main(){
-    //N--;
-    eyy--;
-    printf("%d", eyy);
+    //N--; errorr
+    int64_t new_x = x;
+    int32_t new_y = y;
+    printf("x: %d\n", new_x); 
+    printf("y: %d", new_y); 
 }
