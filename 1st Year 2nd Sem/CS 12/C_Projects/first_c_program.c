82c3a1a2a28eaa6708cc8d42baa94a699ac55799
JacobLaptop <justjacobrosario@gmail.com>
2026-05-07 13:03:26 +0800

    vault backup: 2026-05-07 13:03:26

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -3,14 +3,14 @@
 
 
 #define N 100
-int32_t x = 10;
-int64_t y = 50;
+int16_t x = 123456789;
+int32_t y = 123456789;
 
 
 int main(){
     //N--; errorr
-    int64_t new_x = x;
-    int32_t new_y = y;
+    int32_t new_x = x;
+    int16_t new_y = y;
     printf("x: %d\n", new_x); 
     printf("y: %d", new_y); 
 }
