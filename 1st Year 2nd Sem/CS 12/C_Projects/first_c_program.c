429a166cff3ea0f968be849ab92873dedf5658be
JacobLaptop <justjacobrosario@gmail.com>
2026-05-08 10:24:52 +0800

    vault backup: 2026-05-08 10:24:52

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -6,10 +6,6 @@ int main(){
     scanf("%d", &n);
     int arr[n] = {};
 
-    for (int i = 0; i < n; i++){
-        arr[i] = i;
-    }
-
     for (int k = 0; k < n; k++){
         printf("%d", arr[k]);
     }
