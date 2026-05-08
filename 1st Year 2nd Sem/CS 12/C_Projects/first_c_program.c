e3b442df4ae0b9878eb36ada5ecacf30bfad07ad
JacobLaptop <justjacobrosario@gmail.com>
2026-05-08 10:31:49 +0800

    vault backup: 2026-05-08 10:31:49

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 int main(){
+
+    // declare variable to make a pointer later
     int n;
+
+    // ask the length of array
     printf("%s", "How many elements: ");
     scanf("%d", &n);
+
+    // then declare the array itself
     int arr[n] = {};
 
+    // 
     for (int i = 0; i < n; i++){
-        arr[i] = i;
+        printf("%sth-element (ints onlyy muna): " , i);
+        scanf("%d", &arr[i]);
     }
 
+    // printing purposes
     for (int k = 0; k < n; k++){
         printf("%d\n", arr[k]);
     }
