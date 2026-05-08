eb0820bc7cbd319d1516e63d85443ea59f686f2e
JacobLaptop <justjacobrosario@gmail.com>
2026-05-08 10:25:41 +0800

    vault backup: 2026-05-08 10:25:41

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -3,10 +3,5 @@
 int main(){
     int n;
     printf("%c", "How many elements: ");
-    scanf("%d", &n);
-    int arr[n] = {};
-
-    for (int k = 0; k < n; k++){
-        printf("%d", arr[k]);
-    }
+    
 }
