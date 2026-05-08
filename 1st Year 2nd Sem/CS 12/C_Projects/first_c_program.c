2ac2109144e18deedf5dd0ed8e17a6f9370b205b
JacobLaptop <justjacobrosario@gmail.com>
2026-05-08 10:23:47 +0800

    vault backup: 2026-05-08 10:23:47

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-void increm_each(int *pList, int n) {
-    for (int i = 0; i < n; i ++){
-        pList[i]++;
-    }
-}
-
 int main(){
-    int lis[] = {1, 2, 3};
-    increm_each(lis, 3);
-    for (int k = 0; k < 3; k++){
-        printf("%d\n", lis[k]);
+    int n;
+    printf("%c", "How many elements: ");
+    scanf("%d", &n);
+    int arr[n] = {};
+
+    for (int i = 0; i < n; i++){
+        arr[i] = i;
+    }
+
+    for (int k = 0; k < n; k++){
+        printf("%d", arr[k]);
     }
 }
