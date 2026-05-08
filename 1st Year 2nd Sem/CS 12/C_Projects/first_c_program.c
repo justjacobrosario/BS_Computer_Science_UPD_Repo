799a08cc4e90e65a80abbba9ece2dd457c57127c
JacobLaptop <justjacobrosario@gmail.com>
2026-05-08 10:33:38 +0800

    vault backup: 2026-05-08 10:33:38

--- 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/first_c_program.c
@@ -14,12 +14,13 @@ int main(){
 
     // 
     for (int i = 0; i < n; i++){
-        printf("%sth-element (ints onlyy muna): " , i);
+        printf("%dth-element (ints onlyy muna): " , i);
         scanf("%d", &arr[i]);
     }
 
     // printing purposes
     for (int k = 0; k < n; k++){
-        printf("%d\n", arr[k]);
+        printf("ee");
+        printf("%d", arr[k]);
     }
 }
