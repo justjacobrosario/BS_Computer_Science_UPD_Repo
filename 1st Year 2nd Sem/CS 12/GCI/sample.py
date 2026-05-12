abdefc849adb48ae3dbfc6339fd043e785e6a5c4
JacobLaptop <justjacobrosario@gmail.com>
2026-05-12 23:28:45 +0800

    vault backup: 2026-05-12 23:28:45

--- 1st Year 2nd Sem/CS 12/GCI/sample.py
+++ 1st Year 2nd Sem/CS 12/GCI/sample.py
@@ -39,6 +39,5 @@ pd.set_option("display.max_rows", 35)
 
 def homework(anime_data: pd.DataFrame, metric_column: str, n: int) -> pd.Series:
     anime_data.sort_values(by=metric_column, ascending=False)
-    return anime_data
-
+    pd.qcut(anime_data, 10)
 print(homework(anime_data_raw, "Completed", 10))
