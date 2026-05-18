7237fbb69adde049d6b509f08aa54a426b23c654
JacobLaptop <justjacobrosario@gmail.com>
2026-05-18 20:16:22 +0800

    vault backup: 2026-05-18 20:16:22

---
+++ 1st Year 2nd Sem/CS 12/Elm_Projects/src/prac.elm
@@ -0,0 +1,10 @@
+module Test_drive exposing (..)
+import Dict exposing (Dict)
+
+my_sum : List Int -> Int
+my_sum lst =
+    let
+        my_add next prev =
+            next + prev
+    in
+        List.foldl my_add 0 lst
