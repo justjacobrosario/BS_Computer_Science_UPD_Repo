389954929282d496edd05b5005d126d64f126ad3
JacobLaptop <justjacobrosario@gmail.com>
2026-05-23 14:58:16 +0800

    vault backup: 2026-05-23 14:58:15

--- 1st Year 2nd Sem/CS 12/CS12 Lecture Notes/L24_C_Programming_Basics_IV/C_Programming_Basics_III.md
+++ 1st Year 2nd Sem/CS 12/CS12 Lecture Notes/L24_C_Programming_Basics_IV/C_Programming_Basics_III.md
@@ -1,1 +1,2 @@
-	
+## 1. Dynamically allocated nested arrays
+
--- 1st Year 2nd Sem/CS 12/C_Projects/try.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/try.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *make_range(int n){
-	int *lis = malloc(sizeof(int) * n);
-
-	
-	for (int i = 0; i < n; i++){
-		lis[i] = i;
-	}
-
-	return lis;
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void *make_contiguous_matrix(int rows, int cols, int element_size) {
+  return calloc(rows * cols, element_size);
 }
-
-int main(){
-	int n = 11;
-	int *p = make_range(n);
-
-	for (int i = 0; i < n; i++){
-		printf("%d ", *(p + i));
-	}
-
+ 
+int main() {
+  int (*matrix)[5] = make_contiguous_matrix(3, 5, sizeof(int));
+ 
+  for (int r = 0; r < 3; r++) {
+    printf("\nmatrix[%d] = %p (%p)\n", r, matrix[r], &matrix[r]);
+    for (int c = 0; c < 5; c++) {
+      printf("matrix[%d][%d] = %d (%p)\n", r, c, matrix[r][c], &matrix[r][c]);
+    }
+  }
 }
