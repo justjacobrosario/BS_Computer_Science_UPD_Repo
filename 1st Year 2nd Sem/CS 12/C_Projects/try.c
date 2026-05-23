eed80c2415521038ac9d6a062f00b6d934b6b623
JacobLaptop <justjacobrosario@gmail.com>
2026-05-23 15:19:11 +0800

    vault backup: 2026-05-23 15:19:11

--- 1st Year 2nd Sem/CS 12/C_Projects/try.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/try.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *make_range(int n){
-	int *lis = malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++){
-		lis[i] = i;
-	}
-
-	return lis;
+void *make_2d_grid(int row, int col, int cell_size){
+	int *matrix = calloc((row*col), sizeof(cell_size));
+	return matrix;
 }
 
-int main(){
-	int n = 11;
-	int *p = make_range(n);
+int main() {
+	int r = 2;
+	int c = 3;
+	int (*p)[c] = make_2d_grid(r, c, sizeof(int));
 
-	for (int i = 0; i < n; i++){
-		printf("%d\n", *(p + i));
+	for (int i = 0; i < r; i++ ){
+
+		for (int j = 0; j < c; j++)
+			printf("grid[%d][%d]: %d\n", i, j, p[i][j]);
+
 	}
+
+
 
 }
