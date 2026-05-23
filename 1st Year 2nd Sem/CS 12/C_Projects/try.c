f80dd53fb4087745d4e190beea7345cd8c53d000
JacobLaptop <justjacobrosario@gmail.com>
2026-05-23 12:29:17 +0800

    vault backup: 2026-05-23 12:29:17

--- 1st Year 2nd Sem/CS 12/C_Projects/try.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/try.c
@@ -1,46 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-int main() {
-	int id;
-	char msg[67];
-
-	while (1) {
-		printf("Input id: ");
-		scanf("%d", &id);
-
-		printf("Input message: ");
-		scanf("%s", msg);
-
-		printf("ID: %d, MSG: %s\n", id, msg);
-	}
-}
-*/
-
 void increm_baon(int *baon){
-	*baon = *baon + 1;
+    *baon = *baon + 1;
 }
 
 int main() {
-	
-	typedef struct student Student;
-	struct student {
-		int baon;
-		char name[123];
-		char univ[123];
-	};
 
-	struct student maria = {
-		.name = "Maria Clara",
-		.baon = 76,
-		.univ = "UST"
-	};
+    typedef struct student Student;
 
+    struct student {
+        int baon;
+        char name[123];
+        char univ[123];
+    };
 
-	printf("%d\n", maria.baon);
+    struct student maria = {
+        76,
+        "Maria Clara",
+        "UST"
+    };
 
-	
+    printf("%d\n", maria.baon);
+
+    return 0;
 }
-
-
