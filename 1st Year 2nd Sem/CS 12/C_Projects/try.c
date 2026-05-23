f2022809147cafd8d7f72ce16d54eda379265fae
JacobLaptop <justjacobrosario@gmail.com>
2026-05-23 10:01:24 +0800

    vault backup: 2026-05-23 10:01:24

--- 1st Year 2nd Sem/CS 12/C_Projects/try.c
+++ 1st Year 2nd Sem/CS 12/C_Projects/try.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *create67(){
-	int *p = malloc(sizeof(int));
-	*p = 67;
-	return p;
-}
-
-int *first_negative(int *p, int n){
-	for (int i = 0; i < n; i++){
-		if (*(p + i) < 0) {
-			return (p+i);
-		}
-	}
-	return NULL;
-}
 
 int main() {
 	int x;
 	char y;
-	scanf("%d\n", &x);
-	scanf("%c\n", &y);
+	scanf("x: %d\n", &x);
+	scanf("y: %s\n", &y);
 	printf("%d %c\n", x, y);
 }
 
