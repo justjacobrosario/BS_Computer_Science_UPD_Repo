8998f918cb459873108ed7782ef6ddf9f63ee8ec
JacobLaptop <justjacobrosario@gmail.com>
2026-06-11 08:28:15 +0800

    vault backup: 2026-06-11 08:28:15

--- binary.c
+++ binary.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-#include <time.h>
+#include <math.h>
 
 int main() {
+    int bin;
 
-    srand(time(NULL));
-    printf("%d", rand());
+    printf("Enter Binary: ");
+    scanf("%d\n", &bin);
+
+    int copy = bin;
+
+    int i = 0;
+
+    int curr;
+
+    int res = 0;
+
+    while (copy > 0){
+        curr = copy % 10; //leftmost val
+        res += (curr * pow(2, i));
+
+        i += 1;
+        copy = floor(copy / 10);
+    }
 
 
-    return 0;
+    printf("Digital Output: %d", res);
+
+
 }
