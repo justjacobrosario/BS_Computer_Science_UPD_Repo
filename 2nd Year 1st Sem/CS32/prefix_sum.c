c56a2d4258573415755ec427ca4897b98ae6a711
JacobLaptop <justjacobrosario@gmail.com>
2026-06-22 13:44:02 +0800

    vault backup: 2026-06-22 13:44:02

---
+++ 2nd Year 1st Sem/CS32/prefix_sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+int* prefix_sums(int seq[]){
+    int res[]
+}
+    */
+
+
+int main(){
+    
+    int seq[] = {1, 2, 3};
+
+    int *pseq = &seq;
+    int dummy = 0;
+
+    int len = pseq;
+
+    printf("%d", len);
+    
+
+
+
+    return 0;
+}
