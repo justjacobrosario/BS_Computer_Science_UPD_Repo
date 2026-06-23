873e21ea11b021d88d0b37e03ae7847e60647945
JacobLaptop <justjacobrosario@gmail.com>
2026-06-24 06:12:54 +0800

    vault backup: 2026-06-24 06:12:54

--- 2nd Year 1st Sem/CS32/prefix_sum.c
+++ 2nd Year 1st Sem/CS32/prefix_sum.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    printf("hello");
+    
+    typedef struct DynamicArray{
+        int size;
+        int cap;
+        int* a;
+    } DynamicArray;
+
+    DynamicArray *l = (DynamicArray*)malloc(sizeof(DynamicArray));
+    (*l).size = 0;
+    (*l).cap = 3;
+    (*l).a = (int*)malloc((*l).cap * sizeof(int));
+
+    for (int i = 0; i < ((*l).cap); i++){
+        (*l).a[i] = i;
+    }
+
+    
+    // --------
+
+
+    int try[3] = {0, 1, 2};
+    int i = 0;
+    while (i > (*l).size){
+        printf("%d", try[i]);
+        i ++;
+    }
+
+
+    printf("yes: %d", try[1]);
+    printf("Donneeee");
+
 
 
     return 0;
