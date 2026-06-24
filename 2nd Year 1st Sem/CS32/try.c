b2e38e7740b57db70e0ce4fd0f19653013a93313
JacobLaptop <justjacobrosario@gmail.com>
2026-06-24 10:44:54 +0800

    vault backup: 2026-06-24 10:44:54

--- 2nd Year 1st Sem/CS32/try.c
+++ 2nd Year 1st Sem/CS32/try.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct DynamicArray{
+    int size;
+    int cap;
+    int *arr;
+} DynamicArray;
+
+DynamicArray *make_as_dynamic_arr(int *arr){
+    DynamicArray *d_arr = malloc(sizeof(DynamicArray));
+    (*d_arr).size = 0;
+    (*d_arr).cap = 1;
+    (*d_arr).arr = malloc((*d_arr).cap * sizeof(int));
+
+    return d_arr;
+
+}
+
+void append(DynamicArray *d_arr, int val){
+    if ((*d_arr).size = (*d_arr).cap){
+
+        (*d_arr).size++;
+        (*d_arr).cap++;
+
+        int *temp_arr = malloc(((*d_arr).size++) * sizeof(int));
+
+        int i = 0;
+        while (i < ((*d_arr).size - 1)){
+            temp_arr[i] = (*d_arr).arr[i];
+            i++;
+        }
+
+        temp_arr[((*d_arr).size - 1)] = val;
+        free((*d_arr).arr);
+        (*d_arr).arr = temp_arr;
+        free(temp_arr);
+
+    }
+}
+
 int main(){
 
-    int lis[3] = {2, 2, 3};
-
-    //printf("%d", *lis);
+    int *try_d_arr;
 
 
-    int size = 3;
-    int *arr = malloc(size * sizeof(int));
 
-    arr[0] = 1;
-    arr[1] = 2;
-    arr[2] = 3;
-
-    printf("%d\n", arr[1]);
-
-    // lets try to change the size and copying the values of each element
-
-
-    size = 10;
-    int *temp_arr = malloc(size * sizeof(int));
-    temp_arr[0] = arr[0];
-    temp_arr[1] = arr[1];
-    temp_arr[2] = arr[2];
-    temp_arr[9] = 67;
-
-    free(arr); // this frees up the memory of the previous arr array
-
-    arr = temp_arr;
-    free(temp_arr);
-
-    printf("%d\n", arr[9]);
-    
+    int size = 
 
 
 
