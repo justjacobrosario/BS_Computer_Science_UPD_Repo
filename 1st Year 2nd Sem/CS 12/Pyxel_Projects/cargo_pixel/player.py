1555864b9b9691a5e8b933d97b3638c074a6d6dc
JacobLaptop <justjacobrosario@gmail.com>
2026-06-27 17:05:53 +0800

    vault backup: 2026-06-27 17:05:53

--- 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/player.py
+++ 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/player.py
@@ -3,18 +3,84 @@ from constants import ShipType, Level_to_Ship, Ship_to_Tile
 
 class Player:
 	def __init__(self, x, y, lvl):
-		self._x = x
-		self._y = y
+		self.x = x
+		self.y = y
 		self._lvl = lvl
 		self._ship = Level_to_Ship[lvl]
 		self._ship_name = self._ship.value["name"]
 		self._hp = self._ship.value["hp"]
 		self._speed = self._ship.value["speed"]
 		self._max_cargo = self._ship.value["max_cargo"]
-		self._cargo = 0
+		self._curr_cargo = 0
 		self._fuel = self._ship.value["fuel"]
 		self._curr_fuel = self._fuel
 		self._min_lvl_req = self._ship.value["min_lvl_req"]
 
+	@property
+	def x(self):
+		return self._x
+	
+	@property
+	def y(self):
+		return self._y
 
+	@property
+	def lvl(self):
+		return self._lvl
+
+	@property
+	def ship(self):
+		return self._ship
+
+	@property
+	def ship_name(self):
+		return self._ship_name
+
+	@property
+	def hp(self):
+		return self._hp
+
+	@property
+	def speed(self):
+		return self._speed
+
+	@property
+	def max_cargo(self):
+		return self._max_cargo
+	
+	@property
+	def curr_cargo(self):
+		return self._curr_cargo
+
+	@property
+	def fuel(self):
+		return self._fuel
+	
+	@property
+	def curr_fuel(self):
+		return self._curr_fuel
+
+	@property
+	def min_lvl_req(self):
+		return self._min_lvl_req
+
+	def lvl_up(self):
+		self._lvl += 1
+		self._ship = Level_to_Ship[lvl]
+		self._ship_name = self._ship.value["name"]
+		self._hp = self._ship.value["hp"]
+		self._speed = self._ship.value["speed"]
+		self._max_cargo = self._ship.value["max_cargo"]
+		self._curr_cargo = 0
+		self._fuel = self._ship.value["fuel"]
+		self._curr_fuel = self._fuel
+		self._min_lvl_req = self._ship.value["min_lvl_req"]
+	
+	
+	
+	
+	
+	
+	
+	
 
