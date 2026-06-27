94e85b07f94eaa133075b13257d9b7cb84b81d2c
JacobLaptop <justjacobrosario@gmail.com>
2026-06-27 19:10:42 +0800

    vault backup: 2026-06-27 19:10:42

--- 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/main.py
+++ 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/main.py
@@ -4,5 +4,5 @@ from model import Model
 from view import View
 
 if __name__ == "__main__":
-    controller = Controller(Model(1980, 1080, "southeast map.png"), View())
+    controller = Controller(Model(128, 72, "southeast map.png"), View())
     controller.run_game()
