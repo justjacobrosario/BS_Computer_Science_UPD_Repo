715472bd348b1207b412b262def82a1f5bf8736d
JacobLaptop <justjacobrosario@gmail.com>
2026-06-27 12:46:16 +0800

    vault backup: 2026-06-27 12:46:16

---
+++ 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/world.py
@@ -0,0 +1,33 @@
+from PIL import Image
+
+
+RGB_TO_TILE = {
+    (31, 49, 160):0, # ocean
+    (55, 155, 196):1, # sea
+    (58, 141, 9):2, # grass
+    (197, 212, 92):3, # sand
+    (255, 255, 255):4 # snow
+}
+
+def nearest_tile(curr_rgb):
+'''
+in a pixel's rgb, get the nearest tile from the RGB_TO_TILE dict
+'''
+    closest_tile, best_dist = 0, float("inf")
+    for reference, tile in RGB_TO_TILE.items():
+        dist = sum((curr-ref)**2 for curr, ref in zip(curr_rgb, reference) )
+
+        if dist < best_dist:
+            closest_tile, best_dist = tid, d
+    return closest_tile
+
+
+img = Image.open("southeast map.png").convert("RGB")
+w, h = img.size
+
+map_data = []
+for y in range(h):
+    row = []
+    for x in range(w):
+        rgb = img.getpixel((x, y))
+        row.append(nearest_tile(rgb))
