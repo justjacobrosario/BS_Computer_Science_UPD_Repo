bdea47710f53078c6d92e94c9dab840ff1b188a8
JacobLaptop <justjacobrosario@gmail.com>
2026-06-27 16:51:54 +0800

    vault backup: 2026-06-27 16:51:54

--- 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/constants.py
+++ 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/constants.py
@@ -1,32 +1,64 @@
-from enum import Enum
+from enum import Enum, IntEnum
+
+class Color(IntEnum):
+    BLACK = 0
+    DARK_BLUE = 1
+    DARK_PURPLE = 2
+    DARK_GREEN = 3
+    BROWN = 4
+    DARK_GRAY = 5
+    LIGHT_GRAY = 6
+    WHITE = 7
+    RED = 8
+    ORANGE = 9
+    YELLOW = 10
+    GREEN = 11
+    BLUE = 12
+    LAVENDER = 13
+    PINK = 14
+    PEACH = 15
+
+
+
+class Key_Input(Enum):
+    QUIT = auto()
+    GO_UP = auto()
+    GO_DOWN = auto()
+    GO_LEFT = auto()
+    GO_RIGHT = auto()
+    GO_UP_2 = auto(),
+    GO_DOWN_2 = auto()
+    GO_LEFT_2 = auto()
+    GO_RIGHT_2 = auto()
+    NONE = auto()
 
 class ShipType(Enum):
 	Tugboat = {
 		"name" : "Tugboat",
 		"hp" : 100, 
 		"speed" : 100, 
-		"cargo" : 50, 
+		"max_cargo" : 50, 
 		"fuel" : 100, 
 		"min_lvl_req" : 0}
 	Mini_Bulk_Carriers = {
 		"name" : "Mini-Bulk Carriers",
 		"hp" : 100, 
 		"speed" : 100, 
-		"cargo" : 50, 
+		"max_cargo" : 50, 
 		"fuel" : 100, 
 		"min_lvl_req" : 0}
 	Roro = {
 		"name" : "RORO Ship",
 		"hp" : 100, 
 		"speed" : 100, 
-		"cargo" : 50, 
+		"max_cargo" : 50, 
 		"fuel" : 100, 
 		"min_lvl_req" : 0}
 	Super_Tanker = {
 		"name" : "Super Tanker",
 		"hp" : 100, 
 		"speed" : 100, 
-		"cargo" : 50, 
+		"max_cargo" : 50, 
 		"fuel" : 100, 
 		"min_lvl_req" : 0}
 
