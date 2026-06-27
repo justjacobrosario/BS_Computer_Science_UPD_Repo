746409082fe4e26228734a37a6c2cb1f1ffb010d
JacobLaptop <justjacobrosario@gmail.com>
2026-06-27 13:01:10 +0800

    vault backup: 2026-06-27 13:01:10

--- 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/world.py
+++ 1st Year 2nd Sem/CS 12/Pyxel_Projects/cargo_pixel/world.py
@@ -9,36 +9,43 @@ RGB_TO_TILE = {
     (255, 255, 255):4 # snow
 }
 
-def nearest_tile(curr_rgb):
-    '''
-    in a pixel's rgb, get the nearest tile from the RGB_TO_TILE dict
-    '''
-    closest_tile, best_dist = 0, float("inf")
-    for reference, tile in RGB_TO_TILE.items():
-        dist = sum((curr-ref)**2 for curr, ref in zip(curr_rgb, reference) )
 
-        if dist < best_dist:
-            closest_tile, best_dist = tile, dist
-    return closest_tile
+class world:
+    def __init__(self, map_path):
+        self._map_path = map_path
 
 
-img = Image.open("southeast map.png").convert("RGB")
-w, h = img.size
+    def upd_map(self):
+        def nearest_tile(curr_rgb):
+            '''
+            in a pixel's rgb, get the nearest tile from the RGB_TO_TILE dict
+            '''
+            closest_tile, best_dist = 0, float("inf")
+            for reference, tile in RGB_TO_TILE.items():
+                dist = sum((curr-ref)**2 for curr, ref in zip(curr_rgb, reference) )
 
-map_data = []
-for y in range(h):
-    row = []
-    for x in range(w):
-        rgb = img.getpixel((x, y))
-        row.append(nearest_tile(rgb))
-    map_data.append(row)
+                if dist < best_dist:
+                    closest_tile, best_dist = tile, dist
+            return closest_tile
 
-with open("map_data.py", "w") as f:
-    f.write(f"MAP_W = {w}\n")
-    f.write(f"MAP_H = {w}\n")
-    f.write(f"MAP_DATA = [\n")
-    for r in map_data:
-        f.write(f"    {r},\n")
-    f.write("]\n")
+
+        img = Image.open(self._map_path).convert("RGB")
+        w, h = img.size
+
+        map_data = []
+        for y in range(h):
+            row = []
+            for x in range(w):
+                rgb = img.getpixel((x, y))
+                row.append(nearest_tile(rgb))
+            map_data.append(row)
+
+        with open("map_data.py", "w") as f:
+            f.write(f"MAP_W = {w}\n")
+            f.write(f"MAP_H = {w}\n")
+            f.write(f"MAP_DATA = [\n")
+            for r in map_data:
+                f.write(f"    {r},\n")
+            f.write("]\n")
 
 print("done")
